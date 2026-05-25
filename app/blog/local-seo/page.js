0a969c2f8e9332019e30097fa76dd12d6107d5ab
djsms90 <danny@djs90.com>
2026-05-25 03:28:33 -0700

    Convert all blog posts to Next.js pages with shared layout

---
+++ app/blog/local-seo/page.js
@@ -0,0 +1,19 @@
+import BlogLayout from '../../../components/BlogLayout'
+
+export const metadata = {
+  title: 'The Local SEO Playbook for Niche Service Professionals in 2026 | DJS Marketing Services',
+  description: 'If you serve a local market and you are not ranking in the top 3 locally, you are invisible to the clients who are actively looking for you right now.',
+}
+
+export default function BlogPost() {
+  return (
+    <BlogLayout
+      tag="Local SEO"
+      title="The Local SEO Playbook for Niche Service Professionals in 2026"
+      date="April 7, 2026"
+      readTime="6 min read"
+    >
+      <div dangerouslySetInnerHTML={{ __html: `` }} />
+    </BlogLayout>
+  )
+}
