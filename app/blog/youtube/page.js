9cb30c344e16dcbbef86b384f94797d1b5114598
djsms90 <danny@djs90.com>
2026-05-25 03:28:35 -0700

    Convert all blog posts to Next.js pages with shared layout

---
+++ app/blog/youtube/page.js
@@ -0,0 +1,19 @@
+import BlogLayout from '../../../components/BlogLayout'
+
+export const metadata = {
+  title: 'How Professional Service Providers Are Using YouTube to Generate Inbound Clients | DJS Marketing Services',
+  description: 'The exact strategy we used to take a client from zero to 1M+ views and make YouTube their number one inbound source. Zero cold outreach.',
+}
+
+export default function BlogPost() {
+  return (
+    <BlogLayout
+      tag="Video Marketing"
+      title="How Professional Service Providers Are Using YouTube to Generate Inbound Clients"
+      date="April 3, 2026"
+      readTime="8 min read"
+    >
+      <div dangerouslySetInnerHTML={{ __html: `` }} />
+    </BlogLayout>
+  )
+}
